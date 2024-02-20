6fe4b439306bf2e47375ba9825719825df279fca
MyslivetsOleg <myslivec.oleg@gmail.com>
2024-02-20 12:12:32 +0000

    Added some security

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+db_pass = os.environ.get('db_pass')
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://appadmin:Lio123zxc@192.168.3.248:5432/os_commands"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://appadmin:"+str(db_pass)+"@192.168.3.248:5432/os_commands"
 db = SQLAlchemy(app)
 
 if __name__ == '__main__':
